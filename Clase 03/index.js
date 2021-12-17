3b027653924d091a21a0955ce4f44c06c6ba90be
Nataliadiez <natalia.diez10@hotmail.com>
2021-12-17 20:56:54 -0300

    guardado de campos de formulario y URL estáticas

--- Clase 03/index.js
+++ Clase 03/index.js
@@ -6,52 +6,62 @@ Tomar los datos del form y crear un archivo de texto (txt) con los datos
 puestos en el form.
 El navbar debe llevar a url estáticas creadas con la librería “url” de node */
 
+const urlHome = new URL('http://localhost:8080/home');
+const urlProductos = new URL('http://localhost:8080/productos');
 
 
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var formidable = require("formidable")
+
 var port = 8080
-let contenidoFormulario = `
-    <h1>Formulario</h1>
-        <form action="formularioenviado" method="post">
-            <div>
-                <label for="nombre">Nombre</label>
-                <input type="text" id="nombre" name="nombre">
-            </div>
 
-            <div>
-                <label for="apellido">Apellido</label>
-                <input type="text" id="apellido" name="apellido">
-            </div>
-
-            <div>
-                <label for="telefono">Teléfono</label>
-                <input type="text" id="telefono" name="telefono">
-            </div>
-
-            <div>
-                <button type="submit">Enviar</button>
-            </div>
-        </form>
-    </body>
-</html>
-`
-let encabezadoHtml = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta id="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Formulario</title>
-    </head>
-    <body>
-`
 
 
 http.createServer(function(req,res){
+    let contenidoFormulario = `
+        <h1>Formulario</h1>
+            <form action="formularioenviado" method="post">
+                <div>
+                    <label for="nombre">Nombre</label>
+                    <input type="text" id="nombre" name="nombre">
+                </div>
+
+                <div>
+                    <label for="apellido">Apellido</label>
+                    <input type="text" id="apellido" name="apellido">
+                </div>
+
+                <div>
+                    <label for="telefono">Teléfono</label>
+                    <input type="text" id="telefono" name="telefono">
+                </div>
+
+                <div>
+                    <button type="submit" onClick={sendForm()}>Enviar</button>
+                </div>
+            </form>
+        </body>
+    </html>
+    `
+    let encabezadoHtml = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta id="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Formulario</title>
+        </head>
+        <body>
+        <navbar>
+            <a href="http://localhost:8080/home">Home</a>
+            <a href="http://localhost:8080/productos">Productos</a>
+        </navbar>
+        `
     
+
     if (req.url == "/") {
         fs.appendFile("home.html",encabezadoHtml + contenidoFormulario,function(err){
             if (err) {console.log("error en agregado de contenido HTML")}
@@ -71,8 +81,36 @@ http.createServer(function(req,res){
             })
         })
     } else if (req.url == "/formularioenviado") {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files){            
+            console.log(fields)
+            var datosFormulario = "";
+            for(const campo in fields){
+                datosFormulario += `${campo}: ${fields[campo]}\n`
+            }
+            datosFormulario+= "\n-------------- \n \n"
+            /* Object.keys(fields).forEach(key => {
+                console.log(key)
+                console.log(fields[key])
+            }) */
+            fs.appendFile("datos.txt",datosFormulario, function(err){
+                if (err) throw err;
+            })
+        });
         res.writeHead(200, {"Content-Type":"text/html"});
         res.write("Gracias por enviar tus datos");
+        /* res.write(req.headers.host) */
+        res.end();
+
+    } else if (req.url == "/home"){
+        res.writeHead(200, {'Location': 'https://example.com' + req.url});
+        res.write("Esto es el home");
+        res.end();
+
+    } else if (req.url == "/productos"){
+        
+        res.writeHead(200, {"Content-Type":"text/html"});
+        res.write("Estos son los productos");
         res.end();
     }
     
